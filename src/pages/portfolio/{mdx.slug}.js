f7c1bd9c67a0a39bff3942ba0870e72f5bc92c52
BackstreetCultist <charlie@101a.co.uk>
2022-01-01 17:49:26 +0000

    Fix a bug where posts could be indexed at multiple locations
    This commit fixes a minor bug introduced in the previous, where e.g.
    /blog/hello-world could also be accessed at /portfolio/hello-world/.
    The user is now instead shown a 404 page.

--- src/pages/portfolio/{mdx.slug}.js
+++ src/pages/portfolio/{mdx.slug}.js
@@ -2,30 +2,40 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
+import NotFoundPage from '../404'
 import '../../resources/styles/pages/portfolio/{mdx.slug}.scss'
 
 //data prop stores query result (see below)
 const PortfolioPost = ({ data }) => {
-    const image = <img
-        id="heroimage"
-        alt="{data.mdx.frontmatter.hero_image_alt}"
-        src={data.mdx.frontmatter.hero_image} />
+    if (data.mdx == null){
+        return(
+            <NotFoundPage></NotFoundPage>
+        )
+    }
+    else {
+        const image = <img
+            id="heroimage"
+            alt="{data.mdx.frontmatter.hero_image_alt}"
+            src={data.mdx.frontmatter.hero_image} />
+    
+        return (
+            <Layout pageTitle={data.mdx.frontmatter.title}>
+                <p>{data.mdx.frontmatter.datePublished}</p>
+                {image}
+                <MDXRenderer>
+                    {data.mdx.body}
+                </MDXRenderer>
+            </Layout>
+        )
 
-    return (
-        <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.datePublished}</p>
-            {image}
-            <MDXRenderer>
-                {data.mdx.body}
-            </MDXRenderer>
-        </Layout>
-    )
+    }
 }
 
 //$id automatically added as prop by File System Route API
+//mdx is a node-level check rather than allMdx which is a layer above
 export const query = graphql`
     query ($id: String) {
-        mdx(id: {eq: $id}) {
+        mdx(id: {eq: $id}, fileAbsolutePath: {regex: "/portfolio/"}) {
             frontmatter {
                 title
                 datePublished(formatString: "DD/MM/YYYY")
