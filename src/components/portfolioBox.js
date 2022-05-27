3307fae7aff727ab5a738148c2e2f606e56dba7f
BackstreetCultist <charlie@101a.co.uk>
2022-05-27 15:57:34 +0100

    Fix stacking behaviour

--- src/components/portfolioBox.js
+++ src/components/portfolioBox.js
@@ -6,21 +6,17 @@ const PortfolioBox = ({node}) => {
     return (
         <section className={portfolioBox}>
             <article key={node.id}>
-                <div>
-                    <div className={title}>
-                        <h2>
-                            <Link to={`/portfolio/${node.slug}`} className={navLinkItem}>
-                                {node.frontmatter.title}
-                            </Link>
-                        </h2>
-                    </div>
-                    <img
-                        id="heroimage"
-                        alt={node.frontmatter.hero_image_alt}
-                        src={node.frontmatter.hero_image}
-                    />
-                    <h3>{node.frontmatter.description}</h3>
-                </div>
+                <h2>
+                    <Link to={`/portfolio/${node.slug}`} className={navLinkItem}>
+                        {node.frontmatter.title}
+                    </Link>
+                </h2>
+                <img
+                    id="heroimage"
+                    alt={node.frontmatter.hero_image_alt}
+                    src={node.frontmatter.hero_image}
+                />
+                <h3>{node.frontmatter.description}</h3>
             </article>
         </section>
     )
