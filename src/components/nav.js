894993aeb27b53d1e75adf70fb41a53c57b3953a
BackstreetCultist <charlie@101a.co.uk>
2022-05-31 11:39:10 +0100

    Make whole of nav items clickable

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { navLinks, navLinkItem, navLinkText } from '../resources/styles/components/nav.module.scss'
+import { navLinks, navLinkItem, navLinkText, navLinkSpan } from '../resources/styles/components/nav.module.scss'
 
 const Nav = () => {
     return (
@@ -8,21 +8,25 @@ const Nav = () => {
             <ul className={navLinks}>
                 <li className={navLinkItem}>
                     <Link to="/" className={navLinkText}>
+                        <span className={navLinkSpan}></span>
                         Home
                     </Link>
                 </li>
                 <li className={navLinkItem}>
                     <Link to="/about" className={navLinkText}>
+                        <span className={navLinkSpan}></span>
                         About
                     </Link>
                 </li>
                 <li className={navLinkItem}>
                     <Link to="/blog" className={navLinkText}>
+                        <span className={navLinkSpan}></span>
                         Blog
                     </Link>
                 </li>
                 <li className={navLinkItem}>
                     <Link to="/portfolio" className={navLinkText}>
+                        <span className={navLinkSpan}></span>
                         Portfolio
                     </Link>
                 </li>
