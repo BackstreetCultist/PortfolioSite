7647c40f3fb1d5065ba87bc39387c60c70539388
Charlie Wilkins <charlie@101a.co.uk>
2022-06-13 13:16:51 +0100

    Add three paragraphs to About Me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,31 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-const description = <p>I am a fourth year Computer Science with Artificial Intelligence student at the University of Nottingham</p>
+const description = 
+<div>
+<p>
+I am a recent graduate of the University of Nottingham, earning a degree in
+Computer Science with Artificial Intelligence (with Year in Industry), which is
+certainly a longer degree title than any others I've come across. During my
+studies, I worked for a year at Capital One UK, both as a software and quality
+engineer.
+</p>
+<p>
+My primary interests in computer science are backend software, embedded systems,
+and artificial intelligence. I have experience in a number of programming
+languages, software tools, and development methodologies - my preferred
+languages are Java and Haskell, and I am particularly experienced writing tests
+in Cucumber and Gatling. I am also an experienced technical writer, having
+achieved a strong mark on my final dissertation and written several extended
+pieces of documentation for Capital One.
+</p>
+<p>
+Aside from software engineering, I have a keen interest in music (I play frankly
+too many instruments, none very well), cooking, and science fiction movies and
+books. I fancy myself something of a science fiction writer myself, but only as
+a hobby.
+</p>
+</div>
 
 const AboutPage = () => {
   return (
